682060f191708ed5ef663857e459a7ae5e2533e6
John Hinz <jhinz402@hotmail.com>
2017-07-09 17:45:56 -0400

    Initial commit of Ninject IoC example

---
+++ NinjectIoCExample/Bindings.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectIoCExample
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDependency>().To<DependencyOne>();
+        }
+    }
+}
--- NinjectIoCExample/Program.cs
+++ NinjectIoCExample/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,52 @@ namespace NinjectIoCExample
     {
         static void Main(string[] args)
         {
+            // initialize the 
+            StandardKernel kernel = new StandardKernel();
+            // load IoC configuration from the bindings class
+            kernel.Load(Assembly.GetExecutingAssembly());
+            // resolve dependent class
+            IDependency dependency = kernel.Get<IDependency>();
+
+            Dependent dependent = new Dependent(dependency);
+
+            dependent.DoSomething();
+            Console.ReadLine();
+        }
+    }
+
+    public class Dependent
+    {
+        private IDependency _dependency;
+
+        public Dependent(IDependency dependency)
+        {
+            _dependency = dependency;
+        }
+        public void DoSomething()
+        {
+            _dependency.DoSomeWork();
+        }
+    }
+
+    public interface IDependency
+    {
+        void DoSomeWork();
+    }
+
+    public class DependencyOne : IDependency
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("DependencyOne doing work");
+        }
+    }
+
+    public class DependencyTwo : IDependency
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("DependencyTwo doing some work");
         }
     }
 }
